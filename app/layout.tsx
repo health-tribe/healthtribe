b2c8e413229b4140b5c4910ae2c3c4c995c9133f
KAUSHALCODER123 <132132331+KAUSHALCODER123@users.noreply.github.com>
2025-12-05 19:07:44 +0530

    Fix import case for Header component and update scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import Image from "next/image";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 
 import Footer from "@/components/footer";
 const dmSans = DM_Sans({
@@ -16,14 +16,13 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "Health Tribe",
   description: "Health Tribe",
-   icons: {
+  icons: {
     icon: "/assets/svgs/logo.svg",
   },
-  keywords: ["Health", "Tribe", "Health Tribe", "Health Tribe"],
+  keywords: ["Health", "Tribe", "Health Tribe"],
 };
 
-import Head from 'next/head';
-import Link from "next/link";
+
 
 export default function RootLayout({
   children,
@@ -32,16 +31,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <Link rel="icon" href="/assets/svgs/logo.svg" type="image/svg+xml" />
-      </Head>
-      <Script
-        id="razorpay-checkout-js"
-        src="https://checkout.razorpay.com/v1/checkout.js"
-      />
       <body
         className={`${dmSans.variable} font-dm-sans antialiased`}
       >
+        <Script
+          id="razorpay-checkout-js"
+          src="https://checkout.razorpay.com/v1/checkout.js"
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RQJ7N3QGC4"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RQJ7N3QGC4');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
